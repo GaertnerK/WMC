76f4ee73e05d7d6d179964edacfa787cfc20f645
GaertnerK <gaertnerk22039@sus.htl-grieskirchen.at>
2024-10-03 11:51:40 +0200

    Class Word implemented

---
+++ Minigame/app/src/main/java/at/htlgkr/minigame/Logic.java
@@ -0,0 +1,13 @@
+package at.htlgkr.minigame;
+
+public class Logic {
+    Word word;
+    String[] letters;
+
+    public Logic(String[] letters){
+        word = new Word();
+        this.letters = letters;
+    }
+
+
+}
---
+++ Minigame/app/src/main/java/at/htlgkr/minigame/Word.java
@@ -0,0 +1,9 @@
+package at.htlgkr.minigame;
+
+public class Word {
+    String[] letters;
+
+    public Word(){
+        letters = new String[5];
+    }
+}
